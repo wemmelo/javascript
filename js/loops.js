012649053ab2e792347439046fd1db280a6a9ca9
wellington <wellington@gmail.com>
2019-12-20 16:31:03 -0300

    meio da aula de loops

---
+++ js/loops.js
@@ -0,0 +1,17 @@
+var x=0;
+
+/*while
+    while(x<10){
+    console.log("x é igual a " +x );
+    x++;
+}*/
+
+/*do {} while()
+    do {
+    console.log("x é igual a " +x );
+    x++;
+}
+while(x<=10)
+*/
+
+/* parei em 07:30
