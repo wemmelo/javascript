b5c5e8d480ef711262cc6addab8fe800df977882
Wellington Magno Macedo de Melo <wellington.melo@SERPB.local>
2019-12-20 14:01:35 -0300

    primeiro commit no projeto

---
+++ js/condicionais.js
@@ -0,0 +1,7 @@
+var x =20;
+var y ;
+
+if(x==18){
+    y= "maior";
+}
+console.log(y);
---
+++ js/sintaxe-basica.js
@@ -0,0 +1,37 @@
+//statements terminam com ponto e virgula
+var x = 15;
+var y = 12;
+
+//valores: literais (fixos) e variáveis
+//exemplo de literais
+10;
+10.5;
+
+"olá mundo";
+'olá mundo';
+
+//exemplo de variáveis
+var x;
+
+//operadores
+x = 10;
+
+//expressões
+y+10;
+
+/*
+bloco de comentários
+*/
+
+//identificadores "camelcase"
+
+/* tipos de dados 
+números
+strings
+booleanos
+arrays[]
+objetos{}
+
+undefined
+null (ainda não existe)
+*/
